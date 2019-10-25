76b66347d24d7392013585292871eaf52cc04e57
Alex Ozdemir <aozdemir@hmc.edu>
2019-10-25 00:27:25 -0700

    noshape

--- plots.R
+++ plots.R
@@ -1,8 +1,8 @@
 library(tidyverse)
-data <- read_tsv("out.txt")
+data <- read_tsv("plus-prime.txt")
 split <- gather(data, "stat", "value", -name, -b, -s) %>% filter(stat != "stdev")
 ggplot(split) +
-  geom_point(aes(x = b, y = value, color = name, shape = name)) +
+  geom_point(aes(x = b, y = value, color = name)) +
   facet_wrap(~stat) + labs(
     title = "Size of GCD with others",
     y = "GCD Length (bits)",
@@ -11,6 +11,8 @@ ggplot(split) +
     stat = "Statistic"
   ) + scale_color_discrete(
     name="PRG",
-    breaks=c("inc", "uniform"),
-    labels=c("128b uniform, extended with increment", "fully uniform")
+    breaks=c("inc", "uniform", "plus-prime"),
+    labels=c("128b uniform, extended with increment", "fully uniform", "plus a prime")
   )
+
+ggsave("plot.png", width = 8, height = 5, units = "in")
